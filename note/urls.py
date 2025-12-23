e5aab15cc461be1f2eb70ec720d677a9473352a8
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-12-23 23:37:18 +0330

    new endpoint added /notes/new/

--- note/urls.py
+++ note/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import sse_stream
+from .views import sse_stream, create_note
 
 urlpatterns = [
     path("events/", sse_stream, name="sse_stream"),
+    path("notes/new/", create_note, name="create_new_note")
 ]
